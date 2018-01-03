dc33b2d94b40d0fb87c2fee1d8721c3d17260e7c
Merlin Schumacher <merlin.schumacher@gmail.com>
2018-01-03 16:34:58 +0100

    Bugfix for superflous class

--- Basecamp.cpp
+++ Basecamp.cpp
@@ -26,14 +26,13 @@ bool Basecamp::begin() {
 #endif
 	delay(5000);
 #ifndef BASECAMP_NOMQTT
-	HTTPClient http;
 	//mqtt.setSecure(configuration.get("MQTTHost").toInt());
 	//mqtt.setServerFingerprint(configuration.get("MQTTFingerprint").toInt());
 
 	uint16_t mqttport = configuration.get("MQTTPort").toInt();
 	char* mqtthost = configuration.getCString("MQTTHost");
-	char* mqttuser = configuration.get("MQTTPort").toInt();
-	char* mqttpass = configuration.getCString("MQTTHost");
+	char* mqttuser = configuration.getCString("MQTTUser");
+	char* mqttpass = configuration.getCString("MQTTPass");
 	mqtt.setServer(mqtthost, mqttport);
 	if(mqttuser != "") {
 		mqtt.setCredentials(mqttuser,mqttpass);
@@ -41,6 +40,8 @@ bool Basecamp::begin() {
 
 	mqtt.connect();
 	free(mqtthost);
+	free(mqttuser);
+	free(mqttpass);
 	mqtt.onDisconnect([this](AsyncMqttClientDisconnectReason reason) {
 			MqttReconnect(&mqtt);
 			});
