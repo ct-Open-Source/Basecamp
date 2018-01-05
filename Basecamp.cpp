496586da599fd18ccfef833b3de2e27afbf8e4d5
Merlin Schumacher <merlin.schumacher@gmail.com>
2018-01-05 14:53:30 +0100

    Git is hard, vim too ;)

--- Basecamp.cpp
+++ Basecamp.cpp
@@ -1,4 +1,4 @@
-delay(5000);/*
+/*
    Basecamp - ESP32 library to simplify the basics of IoT projects
    Written by Merlin Schumacher (mls@ct.de) for c't magazin für computer technik (https://www.ct.de)
    Licensed under GPLv3. See LICENSE for details.
