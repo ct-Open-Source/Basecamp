036a7912980f435afe7a940ec7fac5a2b6900c80
Dirk Faust <dirk@faustbande.de>
2018-03-10 13:10:09 +0100

    Add mqttGuard and Interface

---
+++ mqttGuard.hpp
@@ -0,0 +1,70 @@
+#ifndef BASECAMP_MQTT_GUARD_HPP
+#define BASECAMP_MQTT_GUARD_HPP
+
+#include "log.hpp"
+
+#include <functional>
+#include <vector>
+
+/**
+  Helper to guard outgoing mqtt packets.
+  On packet sending, register the packets within this class and
+  pull the empty() function to see if everything has been sent completely.
+*/
+class MqttGuard
+{
+public:
+    /// Typesafety forward of AsyncMqttClients packet_id type
+    using IdType = uint16_t;
+
+    /**
+        Construct a new guard.
+        @param LogCallback Optional log callback.
+     */
+    explicit MqttGuard(basecampLog::LogCallback logCallback = {});
+    ~MqttGuard() = default;
+
+    /**
+        Register a new packet that is going to sent by mqtt.
+        @param packetId Packet-ID returned by AsyncMqttClient::publish()
+     */
+    void registerPacket(IdType packetId);
+
+    /**
+        Unregister a packet after it has been sent successfully.
+        @param packetId Packet-ID returned by AsyncMqttClient::onPublish()
+     */
+    void unregisterPacket(IdType packetId);
+
+    /// Returns the amount of packets waiting to be sent.
+    size_t remainingPacketCount() const;
+
+    /// Returns true if all packets have been sent.
+    bool allSent() const;
+
+    /**
+        Reset to empty state (force).
+        Maybe necessary if getting disconnected.
+     */
+    void reset();
+private:
+    /**
+        Check a packetId for validity.
+        @param packetId Packet-ID to be checked.
+        @return True if the packetId can be safely added.
+     */
+    bool isValidPacketId(IdType packetId) const;
+
+    /// Try to log message if logCallback_ has been set in ctor.
+    void tryLog(basecampLog::Severity severity, const std::string& message);
+
+    /// Try to log message with packetId if logCallback_ has been set in ctor.
+    void tryLog(basecampLog::Severity severity, const std::string& message, IdType packetId);
+
+    /// Optional callback for log-messages
+    basecampLog::LogCallback logCallback_;
+    /// List of all remaining packets
+    std::vector<IdType> packets_;
+};
+
+#endif // #define BASECAMP_MQTT_GUARD_HPP
---
+++ mqttGuardInterface.cpp
@@ -0,0 +1,52 @@
+#include "MqttGuardInterface.hpp"
+
+MqttGuardInterface::MqttGuardInterface(AsyncMqttClient& mqttClient)
+    : mqttClient_(mqttClient)
+    , mqttGuard_(std::make_shared<MqttGuard>())
+{
+    // Default callbacks in case the user does not set one so internally everything is kept fine.
+    mqttOnDisconnect([](AsyncMqttClientDisconnectReason /*reason*/){});
+    mqttOnPublish([](uint16_t /*packetId*/) {});
+}
+
+uint16_t MqttGuardInterface::mqttPublish(const char* topic, uint8_t qos, bool retain,
+        const char* payload, size_t length, bool dup, uint16_t message_id)
+{
+    mqttGuard_->registerPacket(mqttClient_.publish(topic, qos, retain, payload, length, dup, message_id));
+}
+
+AsyncMqttClient& MqttGuardInterface::mqttOnPublish(AsyncMqttClientInternals::OnPublishUserCallback callback)
+{
+    // Intercept the onPublish and make sure that mqttGuard is held alive until the callback is fired.
+    auto mqttGuard = mqttGuard_;
+    return mqttClient_.onPublish([mqttGuard, callback](uint16_t packetId){
+        mqttGuard->unregisterPacket(packetId);
+        if (callback) {
+            callback(packetId);
+        }
+    });
+}
+
+AsyncMqttClient& MqttGuardInterface::mqttOnDisconnect(AsyncMqttClientInternals::OnDisconnectUserCallback callback)
+{
+    // Intercept the onDisconnect and make sure that mqttGuard is held alive until the callback is fired.
+    auto mqttGuard = mqttGuard_;
+    return mqttClient_.onDisconnect([mqttGuard, callback]
+                            (AsyncMqttClientDisconnectReason reason){
+        // Make sure that disconnet respects that there could be no more packets be sent.
+        mqttGuard->reset();
+        if (callback) {
+            callback(reason);
+        }
+    });
+}
+
+bool MqttGuardInterface::mqttAllSent() const
+{
+    return mqttGuard_->allSent();
+}
+
+size_t MqttGuardInterface::mqttRemainingPackets() const
+{
+    return mqttGuard_->remainingPacketCount();
+}
