c29bb96ea10643ef536e17fca0052b09452bd474
Holger Pieta <public@holgerpieta.de>
2018-05-01 11:20:47 +0200

    First configure web server, then start it.

--- Basecamp.cpp
+++ Basecamp.cpp
@@ -191,12 +191,6 @@ bool Basecamp::begin(String fixedWiFiApEncryptionPassword)
 #ifndef BASECAMP_NOWEB
 	if (shouldEnableConfigWebserver())
 	{
-		// Start webserver and pass the configuration object to it
-		// Also pass a Lambda-function that restarts the device after the configuration has been saved.
-		web.begin(configuration, [](){
-			delay(2000);
-			esp_restart();
-		});
 		// Add a webinterface element for the h1 that contains the device name. It is a child of the #wrapper-element.
 		web.addInterfaceElement("heading", "h1", "","#wrapper");
 		web.setInterfaceElementAttribute("heading", "class", "fat-border");
@@ -260,6 +254,12 @@ bool Basecamp::begin(String fixedWiFiApEncryptionPassword)
 		}
 		#endif
 		#endif
+		// Start webserver and pass the configuration object to it
+		// Also pass a Lambda-function that restarts the device after the configuration has been saved.
+		web.begin(configuration, [](){
+			delay(2000);
+			esp_restart();
+		});
 	}
 	#endif
 	Serial.println(showSystemInfo());
